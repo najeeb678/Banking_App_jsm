44a6663fdbc7651ba76b33ebbfba3f6818e2de35
najeeb678 <najibqazi777@gmail.com>
2024-06-26 16:39:38 +0500

    impliment initial file and folder structure

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+export default async function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <main>
+      {children}
+    </main>
+  );
+}
---
+++ app/(root)/layout.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+export default async function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <main>
+      Sidebar
+      {children}
+    </main>
+  );
+}
