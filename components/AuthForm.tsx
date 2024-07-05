fdc1a7269cf4320a897aede1dd5bf4f6b439029b
najeeb678 <najibqazi777@gmail.com>
2024-07-05 16:07:58 +0500

    sign-up and sign-in form

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -1,13 +1,86 @@
 'use client';
+
 import Image from 'next/image'
 import Link from 'next/link'
-// import { useRouter } from 'next/router';
 import React, { useState } from 'react'
+
+import { z } from "zod"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import { Button } from "@/components/ui/button"
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
+import CustomInput from './CustomInput';
+import { authFormSchema } from '@/lib/utils';
+import { Loader2 } from 'lucide-react';
+import { useRouter } from 'next/navigation';
+// import { getLoggedInUser, signIn, signUp } from '@/lib/actions/user.actions';
+// import PlaidLink from './PlaidLink';
 
 const AuthForm = ({ type }: { type: string }) => {
     // const router = useRouter();
     const [user, setUser] = useState(null);
-    const [isLoading, setIsLoading] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+  const formSchema = authFormSchema(type);
+
+  // 1. Define your form.
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      email: "",
+      password: ''
+    },
+  })
+ 
+  // 2. Define a submit handler.
+  const onSubmit = async (data: z.infer<typeof formSchema>) => {
+    // setIsLoading(true);
+console.log("onSubmit", data);
+    // try {
+    //   // Sign up with Appwrite & create plaid token
+      
+    //   if(type === 'sign-up') {
+    //     const userData = {
+    //       firstName: data.firstName!,
+    //       lastName: data.lastName!,
+    //       address1: data.address1!,
+    //       city: data.city!,
+    //       state: data.state!,
+    //       postalCode: data.postalCode!,
+    //       dateOfBirth: data.dateOfBirth!,
+    //       ssn: data.ssn!,
+    //       email: data.email,
+    //       password: data.password
+    //     }
+
+    //     const newUser = await signUp(userData);
+
+    //     setUser(newUser);
+    //   }
+
+    //   if(type === 'sign-in') {
+    //     const response = await signIn({
+    //       email: data.email,
+    //       password: data.password,
+    //     })
+
+    //     if(response) router.push('/')
+    //   }
+    // } catch (error) {
+    //   console.log(error);
+    // } finally {
+    //   setIsLoading(false);
+    // }
+  }
+
   return (
     <section className="auth-form">
     <header className='flex flex-col gap-5 md:gap-8'>
@@ -21,7 +94,7 @@ const AuthForm = ({ type }: { type: string }) => {
           <h1 className="text-26 font-ibm-plex-serif font-bold text-black-1">Horizon</h1>
         </Link>
 
-        {/* <div className="flex flex-col gap-1 md:gap-3">
+        <div className="flex flex-col gap-1 md:gap-3">
           <h1 className="text-24 lg:text-36 font-semibold text-gray-900">
             {user 
               ? 'Link Account'
@@ -36,11 +109,11 @@ const AuthForm = ({ type }: { type: string }) => {
               }
             </p>  
           </h1>
-        </div> */}
+        </div>
     </header>
-    {/* {user ? (
+    {user ? (
       <div className="flex flex-col gap-4">
-        <PlaidLink user={user} variant="primary" />
+        {/* <PlaidLink user={user} variant="primary" /> */}
       </div>
     ): (
       <>
@@ -94,7 +167,7 @@ const AuthForm = ({ type }: { type: string }) => {
           </Link>
         </footer>
       </>
-    )} */}
+    )}
   </section>
   )
 }
